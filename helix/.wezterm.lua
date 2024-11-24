23a9920972ee85dd1034662769f1f7a5e9ac7cc9
Patrik Buhring <patrikbuhring@gmail.com>
2024-11-23 20:19:31 -0500

    Disable CTRL|SHIFT ARROW for wezterm

--- helix/.wezterm.lua
+++ helix/.wezterm.lua
@@ -2,11 +2,11 @@ local wezterm = require 'wezterm'
 
 local config = wezterm.config_builder()
 
-config.window_background_image = 'C:\\Users\\Patrik\\Documents\\bkground2.jpeg'
+config.window_background_image = 'C:\\Users\\Optim\\Documents\\bkground.png'
 config.use_fancy_tab_bar = true
 config.window_decorations = 'RESIZE'
-config.default_prog = { 'pwsh' }
-config.font = wezterm.font('Consolas NF')
+config.default_prog = { 'pwsh', '-c', 'nu' }
+config.font = wezterm.font('Consolas')
 config.font_size = 13 
 config.line_height = 1.1
 
@@ -42,6 +42,28 @@ config.keys = {
     mods = "ALT",
     action = act.ActivatePaneDirection 'Right'
   },
+  
+  {
+    key = 'UpArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },  
+  {
+    key = 'DownArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },  
+  {
+    key = 'LeftArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },  
+  {
+    key = 'RightArrow',
+    mods = 'CTRL|SHIFT',
+    action = wezterm.action.DisableDefaultAssignment,
+  },  
+  
   {
     key = "UpArrow",
     mods = "SHIFT|ALT",
@@ -61,6 +83,28 @@ config.keys = {
     key = "RightArrow",
     mods = "SHIFT|ALT",
     action = act.AdjustPaneSize { 'Right', 5 },
+  },
+  {
+    key = "w",
+    mods = "ALT",
+    action = act.ActivateWindowRelative(1),
+  },
+  {
+    key = "w",
+    mods = "SHIFT|ALT",
+    action = act.ActivateWindowRelative(-1),
+  },
+  {
+    key = "w",
+    mods = "CTRL|SHIFT|ALT",
+    action = wezterm.action.SpawnWindow,
+  },
+  {
+    key = "w",
+    mods = "CTRL|ALT",
+    action = act.PaneSelect {
+      mode = 'SwapWithActive'
+    }
   }
 }
 
